1f0a456d8f4c7176e694bdf8762784bdab328c93
MABAIDUL HOQUE <132288979+Mabaidul-Hoque@users.noreply.github.com>
2024-02-17 21:44:35 +0530

    import problem

--- src/components/HotelsPage/Hotels.jsx
+++ src/components/HotelsPage/Hotels.jsx
@@ -9,9 +9,9 @@ import FmdGoodOutlinedIcon from "@mui/icons-material/FmdGoodOutlined";
 import FmdGoodIcon from "@mui/icons-material/FmdGood";
 import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
 import { toast } from "react-toastify";
-import Autocomplete from "../../ui/Autocomplete";
 import { CheckInOutDate } from "../../ui/CheckInOutDate";
 import AddRooms from "../../ui/AddRooms";
+import Autocomplete from "../../ui/AddRooms";
 
 export const OPTION = [
   { name: "Kolkata, West Bengal" },
